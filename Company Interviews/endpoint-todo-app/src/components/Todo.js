0226acb91af2609a993143513813f434e40d3348
egross <egross@luxurypresence.com>
2024-07-05 14:10:00 -0400

    comments

--- Company Interviews/endpoint-todo-app/src/components/Todo.js
+++ Company Interviews/endpoint-todo-app/src/components/Todo.js
@@ -1,9 +1,9 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react'
 import moment from 'moment'
-import TodoItem from './TodoItem';
+import TodoItem from './TodoItem'
 
 function Todo() {
-  const [data, setData] = useState(null);
+  const [data, setData] = useState(null)
   const url = 'https://944ba3c5-94c3-4369-a9e6-a509d65912e2.mock.pstmn.io/get'
 
   // sort the data by date
@@ -20,46 +20,54 @@ function Todo() {
   // order todos by overdue, remaining and completed
   const muxData = (todos) => {
     const today = moment.now()
-    const mappedTodos = todos.map(todo => {
+    const mappedTodos = todos.map((todo) => {
       if (moment(todo.dueDate).isSameOrBefore(today) && !todo.isComplete) {
         todo.isOverdue = true
       }
       return todo
     })
-    const overdueTodos = mappedTodos.filter(todo => todo.isOverdue).sort(sort)
-    const completedTodos = mappedTodos.filter(todo => todo.isComplete).sort(sort)
-    const remainingTodos = mappedTodos.filter(todo => {
-      return !overdueTodos.includes(todo) && !completedTodos.includes(todo)
-    }).sort(sort)
+    const overdueTodos = mappedTodos.filter((todo) => todo.isOverdue).sort(sort)
+    const completedTodos = mappedTodos
+      .filter((todo) => todo.isComplete)
+      .sort(sort)
+    const remainingTodos = mappedTodos
+      .filter((todo) => {
+        return !overdueTodos.includes(todo) && !completedTodos.includes(todo)
+      })
+      .sort(sort)
 
     return [...overdueTodos, ...remainingTodos, ...completedTodos]
   }
 
   // fetch the todos data
-  function fetchData () {
+  function fetchData() {
     fetch(url, {
       method: 'GET',
       headers: {
-        "X-Api-Key": "PMAK-5ef63db179d23c004de50751-10300736bc550d2a891dc4355aab8d7a5c"
-      }
+        // "X-Api-Key": "PMAK-5ef63db179d23c004de50751-10300736bc550d2a891dc4355aab8d7a5c"
+      },
     })
-      .then(response => response.json())
-      .then(json => {
+      .then((response) => response.json())
+      .then((json) => {
         const todos = muxData(json)
         setData(todos)
       })
-      .catch(error => console.error(error));
+      .catch((error) => console.error(error))
   }
 
   useEffect(() => {
     fetchData()
-  }, []);
+  }, [])
 
   return (
     <div className='todo-container'>
-      {data ? data.map(todo => <TodoItem key={todo.id} todoData={todo}></TodoItem>) : 'Loading...'}
+      {data
+        ? data.map((todo) => (
+            <TodoItem key={todo.id} todoData={todo}></TodoItem>
+          ))
+        : 'Loading...'}
     </div>
-  );
+  )
 }
 
-export default Todo;
+export default Todo
--- Company Interviews/endpoint-todo-app/src/components/TodoItem.js
+++ Company Interviews/endpoint-todo-app/src/components/TodoItem.js
@@ -1,11 +1,10 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react'
 import moment from 'moment'
 
-
-function TodoItem({todoData, handleChange}) {
+function TodoItem({ todoData, handleChange }) {
   const [todo, setTodo] = useState(todoData)
 
-  let {description, dueDate, isOverdue, isComplete, id} = todo
+  let { description, dueDate, isOverdue, isComplete, id } = todo
 
   const classNames = ['todo-item']
   if (isOverdue && !isComplete) {
@@ -15,43 +14,44 @@ function TodoItem({todoData, handleChange}) {
     classNames.push('is-complete')
   }
 
-   function handleChange(e) {
+  function handleChange(e) {
     const action = !!e.target.checked
-    const url = 'https://944ba3c5-94c3-4369-a9e6-a509d65912e2.mock.pstmn.io/patch/'
+    const url =
+      'https://944ba3c5-94c3-4369-a9e6-a509d65912e2.mock.pstmn.io/patch/'
 
     fetch(`${url}${id}`, {
       method: 'PATCH',
       headers: {
-        "X-Api-Key": "PMAK-5ef63db179d23c004de50751-10300736bc550d2a891dc4355aab8d7a5c"
+        // "X-Api-Key": "PMAK-5ef63db179d23c004de50751-10300736bc550d2a891dc4355aab8d7a5c"
       },
       body: JSON.stringify({
-        'isComplete': action
-      })
-    })
-    .then(() => {
-      const updatedTodo = {'isComplete': action}
+        isComplete: action,
+      }),
+    }).then(() => {
+      const updatedTodo = { isComplete: action }
       setTodo({
         ...todo,
-        ...updatedTodo
+        ...updatedTodo,
       })
     })
   }
 
   return (
-    <div className={classNames.join(" ")}>
+    <div className={classNames.join(' ')}>
       <div className='first-half-todo'>
         <input
           className='checkbox'
           type='checkbox'
           checked={isComplete}
           onChange={handleChange}
-          >
-        </input>
+        ></input>
         <p className='todo-description'>{description}</p>
       </div>
-      <p className={`${dueDate ? "todo-due-date": ""}`}>{dueDate? moment(dueDate).format('MM/DD/YYYY') : ''}</p>
+      <p className={`${dueDate ? 'todo-due-date' : ''}`}>
+        {dueDate ? moment(dueDate).format('MM/DD/YYYY') : ''}
+      </p>
     </div>
-  );
+  )
 }
 
-export default TodoItem;
+export default TodoItem
